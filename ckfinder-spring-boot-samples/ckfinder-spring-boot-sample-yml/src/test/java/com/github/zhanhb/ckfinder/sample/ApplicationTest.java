5d48539ddd83234398e810b9b2f62e3b0065165c
zhanhb <zhanhb88@gmail.com>
2017-03-18 00:54:04 +0800

    Add sample project

---
+++ ckfinder-spring-boot-samples/ckfinder-spring-boot-sample-yml/src/test/java/com/github/zhanhb/ckfinder/sample/ApplicationTest.java
@@ -0,0 +1,18 @@
+package com.github.zhanhb.ckfinder.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTest {
+
+  @Test
+  public void test() {
+  }
+
+}
