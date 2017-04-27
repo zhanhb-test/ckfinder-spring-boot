870282d2d86e639a6b91b757632f4c99d84c1a04
zhanhb <zhanhb88@gmail.com>
2017-04-28 02:55:07 +0800

    Add maven wrapper

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  mvn -B -Duser.name=zhanhb clean deploy -s .travis/settings.xml $*
+  ./mvnw -B -Duser.name=zhanhb clean deploy -s .travis/settings.xml $*
 else
-  mvn -B -Duser.name=zhanhb install $*
+  ./mvnw -B -Duser.name=zhanhb install $*
 fi
 
