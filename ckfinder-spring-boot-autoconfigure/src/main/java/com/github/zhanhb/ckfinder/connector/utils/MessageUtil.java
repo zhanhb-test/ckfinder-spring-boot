836fb7a247caff19a6179e52ab62de6ac581f7e9
zhanhb <zhanhb88@gmail.com>
2017-08-22 10:24:08 +0800

    Work with locale.forLanguageTag

--- ckfinder-spring-boot-autoconfigure/src/main/java/com/github/zhanhb/ckfinder/connector/utils/MessageUtil.java
+++ ckfinder-spring-boot-autoconfigure/src/main/java/com/github/zhanhb/ckfinder/connector/utils/MessageUtil.java
@@ -13,10 +13,6 @@ package com.github.zhanhb.ckfinder.connector.utils;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Error utils.
@@ -27,8 +23,6 @@ public enum MessageUtil {
 
   private static final String BUNDLE_NAME = MessageUtil.class.getPackage()
           .getName().concat(".LocalStrings");
-  private static final Logger log = LoggerFactory.getLogger(MessageUtil.class);
-  private final ConcurrentMap<String, Locale> map = new ConcurrentHashMap<>(16);
 
   /**
    * Gets error message by locale code.
@@ -39,26 +33,10 @@ public enum MessageUtil {
    */
   public String getMessage(String lang, int errorCode) {
     try {
-      return ResourceBundle.getBundle(BUNDLE_NAME, getLocale(lang)).getString(Integer.toString(errorCode));
+      return ResourceBundle.getBundle(BUNDLE_NAME, Locale.forLanguageTag(lang)).getString(Integer.toString(errorCode));
     } catch (RuntimeException ex) {
       return "";
     }
-  }
-
-  private Locale getLocale(String lang) {
-    return map.computeIfAbsent(lang, lang1 -> {
-      String[] split = lang1.split("[-_]", 3);
-      switch (split.length) {
-        case 1:
-          return new Locale(split[0]);
-        case 2:
-          return new Locale(split[0], split[1]);
-        case 3:
-          return new Locale(split[0], split[1], split[2]);
-      }
-      log.error("unknown locale '{}'", lang1);
-      return Locale.ROOT;
-    });
   }
 
 }
