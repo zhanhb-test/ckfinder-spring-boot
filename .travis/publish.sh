fdd6b2151a6f45cb599e018ff81bbfe7d61f92c3
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-04-28 06:34:19 +0800

    manifest entry Built-By

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./mvnw -B -Duser.name=zhanhb clean deploy -s .travis/settings.xml $*
+  ./mvnw -B clean deploy -s .travis/settings.xml $*
 else
-  ./mvnw -B -Duser.name=zhanhb install $*
+  ./mvnw -B install $*
 fi
-
