d05dfbff4d4004f9c59ae8685f53eb2f7d5ba5d7
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-02 16:35:41 +0800

    maven options

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./mvnw -B clean deploy -s .travis/settings.xml $*
+  ./mvnw -s .travis/settings.xml "$@" deploy
 else
-  ./mvnw -B install $*
+  ./mvnw "$@" install
 fi
