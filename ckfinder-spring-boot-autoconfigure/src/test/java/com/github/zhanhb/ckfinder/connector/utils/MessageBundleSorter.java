0dcfba7d2d72c26db772c775c9770571d6020de6
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-07 12:48:13 +0800

    Update MessageBundleSorter.java

--- ckfinder-spring-boot-autoconfigure/src/test/java/com/github/zhanhb/ckfinder/connector/utils/MessageBundleSorter.java
+++ ckfinder-spring-boot-autoconfigure/src/test/java/com/github/zhanhb/ckfinder/connector/utils/MessageBundleSorter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
-import java.net.URI;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,49 +12,46 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Properties;
-import java.util.Vector;
 
 public class MessageBundleSorter {
 
+  @SuppressWarnings("UseOfSystemOutOrSystemErr")
   public static void main(String[] args) throws IOException {
-    URI uri = URI.create(MessageBundleSorter.class.getResource("LocalStrings.properties").toExternalForm());
-    Path parent = Paths.get(uri).getParent();
-    try (DirectoryStream<Path> stream = Files.newDirectoryStream(parent)) {
+    String pkg = MessageBundleSorter.class.getPackage().getName().replace('.', '/');
+    Path parent = Paths.get("src/main/resources", pkg);
+    DirectoryStream.Filter<Path> filter = pth
+            -> pth.getFileName().toString().matches("LocalStrings.*\\.properties");
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(parent, filter)) {
       stream.forEach(p -> {
-        if (p.getFileName().toString().endsWith(".properties")) {
-          System.out.println(p);
-          try {
-            Properties properties = new PropertiesImpl();
-            try (InputStream is = Files.newInputStream(p)) {
-              properties.load(is);
-            }
-            try (OutputStream is = Files.newOutputStream(p)) {
-              properties.store(is, null);
-            }
-          } catch (IOException ex) {
-            throw new UncheckedIOException(ex);
+        System.out.println(p);
+        try {
+          Properties properties = new KeySortedProperties();
+          try (InputStream is = Files.newInputStream(p)) {
+            properties.load(is);
           }
+          try (OutputStream is = Files.newOutputStream(p)) {
+            properties.store(is, null);
+          }
+        } catch (IOException ex) {
+          throw new UncheckedIOException(ex);
         }
       });
     }
   }
 
-  private static class PropertiesImpl extends Properties {
+  @SuppressWarnings("CloneableImplementsClone")
+  private static class KeySortedProperties extends Properties {
 
     private static final long serialVersionUID = 0L;
 
     @Override
     public Enumeration<Object> keys() {
       @SuppressWarnings("UseOfObsoleteCollectionType")
-      Vector<Object> v = new Vector<>(Collections.list(super.keys()));
+      java.util.Vector<Object> v = new java.util.Vector<>(Collections.list(super.keys()));
       v.sort(Comparator.comparing(Object::toString, Comparator.comparingInt(Integer::parseInt)));
       return v.elements();
     }
 
-    @Override
-    public Object clone() {
-      return super.clone();
-    }
-
   }
+
 }
