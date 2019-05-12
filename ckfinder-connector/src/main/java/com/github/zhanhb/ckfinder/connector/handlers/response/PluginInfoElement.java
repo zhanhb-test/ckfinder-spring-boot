3c2877397e9b7044d05c5aa6e81e11d3ad86a48c
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-12 14:40:06 +0800

    Add annotations to PluginInfoElement

--- ckfinder-connector/src/main/java/com/github/zhanhb/ckfinder/connector/handlers/response/PluginInfoElement.java
+++ ckfinder-connector/src/main/java/com/github/zhanhb/ckfinder/connector/handlers/response/PluginInfoElement.java
@@ -1,15 +1,23 @@
 package com.github.zhanhb.ckfinder.connector.handlers.response;
 
 import java.util.Map;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAnyAttribute;
+import javax.xml.bind.annotation.XmlType;
 import javax.xml.namespace.QName;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Singular;
 import lombok.Value;
 
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Builder(builderClassName = "Builder")
-@Value
 @SuppressWarnings("FinalClass")
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType
+@Value
 public class PluginInfoElement {
 
   @Singular
