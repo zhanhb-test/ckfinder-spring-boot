cb9a2cbd7a23a17f949a637f77737ff149a2d521
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-05-16 02:18:09 +0800

    Execute script file directly

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if git config --get remote.origin.url 2>&1 | grep -F github.com/zhanhb/ckfinder-spring-boot -q && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   ./mvnw -s .travis/settings.xml "$@" deploy
 else
   ./mvnw "$@" install
