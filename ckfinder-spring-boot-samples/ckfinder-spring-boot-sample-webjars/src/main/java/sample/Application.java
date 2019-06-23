f9bccecbb1d534c804b62d9a33604388e1e91e48
zhanhb <6323014+zhanhb@users.noreply.github.com>
2019-06-24 05:10:31 +0800

    Log visit context when starting with main class

--- ckfinder-spring-boot-samples/ckfinder-spring-boot-sample-webjars/src/main/java/sample/Application.java
+++ ckfinder-spring-boot-samples/ckfinder-spring-boot-sample-webjars/src/main/java/sample/Application.java
@@ -1,14 +1,25 @@
 package sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerApplicationContext;
+import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    ApplicationContext context = SpringApplication.run(Application.class, args);
+    if (context instanceof WebServerApplicationContext) {
+      WebServer webServer = ((WebServerApplicationContext) context).getWebServer();
+      log.info("http://localhost:{}{}", webServer.getPort(), context.getApplicationName());
+    }
   }
 
 }
